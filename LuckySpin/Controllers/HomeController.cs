03be78c9f90b13e21a1091faf4b37f58c35eab91
yy-bird <guoqiang2648@gmail.com>
2018-06-06 01:37:45 +0800

    add register page

--- LuckySpin/Controllers/HomeController.cs
+++ LuckySpin/Controllers/HomeController.cs
@@ -27,19 +27,5 @@ namespace LuckySpin.Controllers
 
             return RedirectToAction("Index");
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
